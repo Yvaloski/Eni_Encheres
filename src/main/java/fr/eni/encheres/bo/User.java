a895bdaf80504bf88771ea052789433daf22c793
FleuryEdwin <edwin.fleury2024@campus-eni.fr>
2024-07-09 09:40:34 +0200

    Creation class user

---
+++ src/main/java/fr/eni/encheres/bo/User.java
@@ -0,0 +1,179 @@
+package fr.eni.encheres.bo;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int idUser;
+
+    @Column
+    private String pseudo;
+
+    @Column
+    private String famillyName;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String address;
+
+    @Column
+    private String postalCode;
+
+    @Column
+    private String city;
+
+    @Column
+    private String password;
+
+    @Column
+    private int credit;
+
+    @Column
+    private boolean admin;
+
+    public User(int idUser, String pseudo, String famillyName, String name, String email, String phone, String address, String postalCode, String city, String password, int credit, boolean admin) {
+        this.idUser = idUser;
+        this.pseudo = pseudo;
+        this.famillyName = famillyName;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.password = password;
+        this.credit = credit;
+        this.admin = admin;
+    }
+
+    public User() {
+
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getFamillyName() {
+        return famillyName;
+    }
+
+    public void setFamillyName(String famillyName) {
+        this.famillyName = famillyName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "idUser=" + idUser +
+                ", pseudo='" + pseudo + '\'' +
+                ", famillyName='" + famillyName + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", city='" + city + '\'' +
+                ", password='" + password + '\'' +
+                ", credit=" + credit +
+                ", admin=" + admin +
+                '}';
+    }
+}
+
+
+
