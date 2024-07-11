37d09dabca05e873a9a6359645855fb265a4aa35
Yval <yalorvellec@gmail.com>
2024-07-11 15:28:18 +0200

    Conflit merge resolved

---
+++ src/main/java/fr/eni/encheres/bll/UserService.java
@@ -0,0 +1,26 @@
+package fr.eni.encheres.bll;
+
+import fr.eni.encheres.bo.User;
+
+import java.util.List;
+
+public interface UserService {
+
+     void addUser(User user);
+
+     List<User> getUsers();
+
+     User getUserById(long id);
+
+     void deleteUser(long id);
+
+     void updateUser(User user);
+
+     void setActiveStatusByIdUser (long id, boolean active);
+
+     User getUserbyUsernameOrEmail(String username, String email);
+
+     Boolean existsByEmail(String email);
+
+     Boolean existsByUsername(String username);
+}
