e54a14f837b1a348127b37f0f01b3cdb1d569150
Yval <yalorvellec@gmail.com>
2024-07-12 14:56:42 +0200

    Add ProductDto and modify ProductController (not finished )

--- src/main/java/fr/eni/encheres/controllers/ProductController.java
+++ src/main/java/fr/eni/encheres/controllers/ProductController.java
@@ -1,7 +1,11 @@
 package fr.eni.encheres.controllers;
 
 import fr.eni.encheres.bll.services.ProductService;
+import fr.eni.encheres.bll.services.UserService;
+import fr.eni.encheres.bo.Category;
 import fr.eni.encheres.bo.Product;
+import fr.eni.encheres.bo.User;
+import fr.eni.encheres.dtos.ProductDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,9 +15,11 @@ import java.util.List;
 public class ProductController {
 
     ProductService productService;
+    UserService userService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, UserService userService) {
         this.productService = productService;
+        this.userService = userService;
     }
 
     @GetMapping("/")
@@ -28,7 +34,23 @@ public class ProductController {
     }
 
     @PostMapping("/add")
-    public Product addProduct(@RequestBody Product product) {
+    public Product addProduct(@RequestBody ProductDto productDto) {
+        Product product = new Product();
+        product.setNameProduct(productDto.getNameProduct());
+        product.setDescriptionProduct(productDto.getDescriptionProduct());
+        product.setAuctionStart(productDto.getAuctionStart());
+        product.setAuctionEnd(productDto.getAuctionEnd());
+        product.setStartPrice(productDto.getStartPrice());
+        product.setFinalPrice(productDto.getFinalPrice());
+
+        User seller = userService.getUserById(productDto.getSellerId());
+        product.setSeller(seller);
+
+//        Category category = productService. productDto.getCategoryId();
+//        category.setIdCat(productDto.getCategoryId());
+//        product.setCategory(productDto.getCategoryId());
+
+        product.setSaleState(productDto.getSaleState());
         productService.addProduct(product);
         return product;
     }
---
+++ src/main/java/fr/eni/encheres/dtos/ProductDto.java
@@ -0,0 +1,96 @@
+package fr.eni.encheres.dtos;
+
+import java.time.LocalDate;
+
+public class ProductDto {
+    private int idProduct;
+    private String nameProduct;
+    private String descriptionProduct;
+    private LocalDate auctionStart;
+    private LocalDate auctionEnd;
+    private int startPrice;
+    private int finalPrice;
+    private int sellerId;
+    private int categoryId;
+    private String saleState;
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public String getDescriptionProduct() {
+        return descriptionProduct;
+    }
+
+    public void setDescriptionProduct(String descriptionProduct) {
+        this.descriptionProduct = descriptionProduct;
+    }
+
+    public LocalDate getAuctionStart() {
+        return auctionStart;
+    }
+
+    public void setAuctionStart(LocalDate auctionStart) {
+        this.auctionStart = auctionStart;
+    }
+
+    public LocalDate getAuctionEnd() {
+        return auctionEnd;
+    }
+
+    public void setAuctionEnd(LocalDate auctionEnd) {
+        this.auctionEnd = auctionEnd;
+    }
+
+    public int getStartPrice() {
+        return startPrice;
+    }
+
+    public void setStartPrice(int startPrice) {
+        this.startPrice = startPrice;
+    }
+
+    public int getFinalPrice() {
+        return finalPrice;
+    }
+
+    public void setFinalPrice(int finalPrice) {
+        this.finalPrice = finalPrice;
+    }
+
+    public int getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(int sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getSaleState() {
+        return saleState;
+    }
+
+    public void setSaleState(String saleState) {
+        this.saleState = saleState;
+    }
+}
