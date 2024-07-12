eb970eaf60d192962201ca62d81db2ef7a222dec
Maxime PLANCHER <maxime.plancher2022@campus-eni.fr>
2024-07-12 13:42:01 +0200

    Contraintes de validation ajoutées User

--- src/main/java/fr/eni/encheres/dtos/RegisterUserDto.java
+++ src/main/java/fr/eni/encheres/dtos/RegisterUserDto.java
@@ -1,18 +1,40 @@
 package fr.eni.encheres.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+
 public class RegisterUserDto {
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message="Email must be of email type")
     private String email;
-
+    @NotBlank(message = "Password cannot be blank")
+    @Length(min = 3, max = 20, message = "Password must be between 3-20 characters")
     private String password;
-
+    @NotBlank(message = "Username cannot be blank")
+    @Length(min = 3, max = 20, message = "Username must be between 3-20 characters")
     private String userName;
+    @NotBlank(message = "Firstname cannot be blank")
+    @Length(min = 3, max = 20, message = "Firstname must be between 3-20 characters")
     private String firstName;
+    @NotBlank(message = "Familyname cannot be blank")
+    @Length(min = 3, max = 20, message = "Familyname must be between 3-20 characters")
     private String familyName;
+    @Pattern(regexp = "0[0-9]{9}", message = "The phone number must follow the standard phone number format : start with 0 and followed by 9 numbers")
     private String phone;
+    @NotBlank(message = "Address cannot be blank")
+    @Length(min = 5, max = 100, message = "Address must be between 5 and 100 characters")
     private String address;
+    @NotBlank(message = "City cannot be blank")
+    @Length(min = 3, max = 50, message = "City must be between 3-50 characters")
     private String city;
     private String state;
+    @NotBlank(message = "Postal Code cannot be blank")
+    @Pattern(regexp = "[0-9]{5,6}", message = "The postal code must be composed of 5 or 6 numbers")
     private String postalCode;
+    @NotBlank(message = "Country cannot be blank")
+    @Length(min = 3, max = 20, message = "Country must be between 3-20 characters")
     private String country;
 
 
