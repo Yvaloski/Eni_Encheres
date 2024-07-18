f7d7d94263c8797914bef504fd68930f58e826f7
Maxime PLANCHER <maxime.plancher2022@campus-eni.fr>
2024-07-18 15:43:12 +0200

    retrait import inutilisé

--- src/main/java/fr/eni/encheres/dal/BidRepository.java
+++ src/main/java/fr/eni/encheres/dal/BidRepository.java
@@ -3,10 +3,9 @@ package fr.eni.encheres.dal;
 import fr.eni.encheres.bo.Bid;
 import fr.eni.encheres.bo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface BidRepository extends JpaRepository<Bid, Long> {
     List<Bid> findByProductIdProduct(long idProduct);
